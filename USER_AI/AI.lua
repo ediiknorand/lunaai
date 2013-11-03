6122910f80806af074de810cfbae8aa64f62b47b
Edii Knorand <edii@patchu.knowledge>
2013-11-03 03:46:05 -0200

    profile/default.lua profile now is the default profile

--- USER_AI/AI.lua
+++ USER_AI/AI.lua
@@ -3,7 +3,7 @@ me = require(AI_PATH.."state.lua")
 
 function AI(myid)
   if(not me.loadedProfile()) then
-    me.loadProfile(AI_PATH.."profiles/farmer-antiks.lua", myid)
+    me.loadProfile(AI_PATH.."profiles/default.lua", myid)
   end
   me.run()
 end
