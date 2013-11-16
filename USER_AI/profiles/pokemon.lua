35b3ca3240e8928bafa207d120caf02b55db4ea5
Edii Knorand <edii@patchu.knowledge>
2013-11-16 16:56:51 -0200

    First step to adapt the pok profile for others homunculi

--- USER_AI/profiles/pokemon.lua
+++ USER_AI/profiles/pokemon.lua
@@ -2,22 +2,30 @@ require "./AI/USER_AI/const.lua"
 require "./AI/USER_AI/util.lua"
 require "./AI/USER_AI/states/commands.lua"
 require "./AI/USER_AI/states/idle.lua"
-
--- Profile for a "pokemon"-type event
+require "./AI/USER_AI/states/atk.lua"
 
 Profile = {}
+-- new states
+function stPokLifAtk(myid, target)
+  stLifAttack(myid, target)
+  local homtype = GetV(V_HOMUNTYPE, target)
+  if(isHom(target, LIF, LIF2, LIF_H, LIF_H2) and GetV(V_MOTION, target) == MOTION_SKILL) then
+    SkillObject(myid, 5, 8002, myid)
+  end
+end
 
+-- ftran
 local ftran = {}
 ftran[stIdle] =
   function (myid)
     local actors = getActors(V_TARGET, V_HOMUNTYPE)
     local owner = GetV(V_OWNER, myid)
-    local d = 100
+    local d = 6
     local p_id = 0
     local a_d
     for id, a in pairs(actors) do
       if(a.target == myid or a.target == owner) then
-        return stAttackCMD, {myid, id}
+        return stPokLifAtk, {myid, id}
       end
       a_d = getDistance2(myid, id)
       if(a_d <= d and id >= 100000000 and a.homuntype >= 1 and a.homuntype <= 16 and id ~= myid) then
@@ -26,7 +34,7 @@ ftran[stIdle] =
       end
     end
     if(p_id ~= 0) then
-      return stAttackCMD, {myid, p_id}
+      return stPokLifAtk, {myid, p_id}
     end
     local motion = GetV(V_MOTION, owner)
     if(motion == MOTION_MOVE) then
@@ -45,11 +53,12 @@ ftran[stIdleFollow] =
     return nil, nil
   end
 
-ftran[stAttackCMD] =
+ftran[stPokLifAtk] =
   function (myid, target)
     if(isDead(target)) then
       return stFollowCMD, {myid}
     end
+    local x,y = GetV(V_POSITION, target)
     return nil, nil
   end
 
@@ -59,7 +68,7 @@ ftran[stMoveCMD] = nil
 -- ...
 Profile.command = {
   [MOVE_CMD] = stMoveCMD,
-  [ATTACK_CMD] = stAttackCMD,
+  [ATTACK_CMD] = stPokLifAtk,
   [FOLLOW_CMD] = {follow = stFollowCMD, unfollow = stIdle}
 }
 
