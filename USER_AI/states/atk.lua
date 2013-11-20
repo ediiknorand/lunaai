a63edffe621b1f69608bb061e68dd5bc80cbf695
Edii Knorand <edii@patchu.knowledge>
2013-11-20 01:57:36 -0200

    Added new attack functions

--- USER_AI/states/atk.lua
+++ USER_AI/states/atk.lua
@@ -1,4 +1,22 @@
-function stLifAttack(myid, target)
+function stLifAttack(myid, target, ...)
+  local x,y = GetV(V_POSITION, target)
+  Move(myid, x, y)
+  Attack(myid, target)
+end
+
+function stVanilmirthAttack(myid, target, ...)
+  local x,y = GetV(V_POSITION, target)
+  Move(myid, x, y)
+  Attack(myid, target)
+end
+
+function stAmistrAttack(myid, target, ...)
+  local x,y = GetV(V_POSITION, target)
+  Move(myid, x, y)
+  Attack(myid, target)
+end
+
+function stFilirAttack(myid, target, ...)
   local x,y = GetV(V_POSITION, target)
   Move(myid, x, y)
   Attack(myid, target)
