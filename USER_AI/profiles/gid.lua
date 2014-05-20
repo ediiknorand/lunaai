da06451ad26967a46f671f2189cf05926cfd6a0e
Edii Knorand <edii@patchu.knowledge>
2014-05-20 01:16:32 -0300

    Added some useful profiles

---
+++ USER_AI/profiles/gid.lua
@@ -0,0 +1,142 @@
+require "./AI/USER_AI/const.lua"
+require "./AI/USER_AI/util.lua"
+require "./AI/USER_AI/states/idle.lua"
+require "./AI/USER_AI/states/commands.lua"
+
+Profile = {}
+
+-- Private
+local out_ = nil
+local jobNames = {
+ [0] = "Novice",
+ [1] = "Swordman",
+ [2] = "Magician",
+ [3] = "Archer",
+ [4] = "Acolyte",
+ [5] = "Merchant",
+ [6] = "Thief",
+
+ [7] = "Knight",
+ [8] = "Priest",
+ [9] = "Wizard",
+ [10] = "Blacksmith",
+ [11] = "Hunter",
+ [12] = "Assassin",
+ [13] = "Knight riding a peco",
+
+ [14] = "Crusader",
+ [15] = "Monk",
+ [16] = "Sage",
+ [17] = "Rogue",
+ [18] = "Alchemist",
+ [19] = "Bard",
+ [20] = "Dancer",
+ [21] = "Crusader riding a peco",
+
+ [23] = "Super Novice",
+ [24] = "Gunslinger",
+ [25] = "Ninja",
+
+ [4001] = "High Novice",
+ [4002] = "High Swordman",
+ [4003] = "High Magician",
+ [4004] = "High Archer",
+ [4005] = "High Acolyte",
+ [4006] = "High Merchant",
+ [4007] = "High Thief",
+
+ [4008] = "Lord Kngiht",
+ [4009] = "High Priest",
+ [4010] = "High Wizard",
+ [4011] = "Whitesmith",
+ [4012] = "Sniper",
+ [4013] = "Assassin Cross",
+ [4014] = "Lord Knight riding a peco",
+
+ [4015] = "Paladin",
+ [4016] = "Champion",
+ [4017] = "Professor",
+ [4018] = "Stalker",
+ [4019] = "Creator",
+ [4020] = "Clown",
+ [4021] = "Gypsy",
+ [4022] = "Paladin riding a peco",
+
+ [4046] = "Taekwon",
+ [4047] = "Star Gladiator",
+ [4048] = "Flying Star Gladiator",
+ [4049] = "Soul Linker"
+}
+
+
+-- Functions
+local function classOf(actor) -- requires V_HOMUNTYPE
+  if isHom(actor.id) then
+    if isHom(actor.id, LIF, LIF2, LIF_H, LIF_H2)  then
+      return "Lif"
+    elseif isHom(actor.id, AMISTR, AMISTR2, AMISTR_H, AMISTR_H2)  then
+      return "Amistr"
+    elseif isHom(actor.id, FILIR, FILIR2, FILIR_H, FILIR_H2)  then
+      return "Filir"
+    elseif isHom(actor.id, VANILMIRTH, VANILMIRTH2, VANILMIRTH_H, VANILMIRTH_H2)  then
+      return "Vanilmirth"
+    end
+  elseif isMob(actor.id) then
+    return "Mob("..actor.homuntype..")"
+  elseif actor.id >= 100000000 then
+    if actor.homuntype >= 1000 then
+      return "Pet("..actor.homuntype..")"
+    elseif actor.homuntype == 45 then
+      return "Warp"
+    else
+      return "NPC("..actor.homuntype..")"
+    end
+  end
+  if jobNames[actor.homuntype] then
+    return jobNames[actor.homuntype]
+  end
+  return "Player("..actor.homuntype..")"
+end
+
+-- ftran
+local ftran = {}
+
+ftran[stIdle] =
+  function (myid)
+    if not out_ then
+      return stIdleFollow,{myid}
+    end
+    out_:write("- - - - - - - - -\n")
+    local owner = GetV(V_OWNER, myid)
+    getActors(V_HOMUNTYPE, V_POSITION,
+      function (act, aid)
+        out_:write(aid.."(x:".. act[aid].x ..", y:".. act[aid].y ..") : is a ".. classOf(act[aid]))
+	if aid == myid then
+	  out_:write("(Me)")
+	end
+	if aid == owner then
+	  out_:write("(You)")
+	end
+	out_:write("\n")
+      end)
+    out_:flush()
+    return stIdleFollow,{myid}
+  end
+
+-- Init and command
+
+Profile.command = {
+  [MOVE_CMD] = stMoveCMD,
+  [ATTACK_CMD] = stAttackCMD,
+  [FOLLOW_CMD] = {follow = stIdle, unfollow = stIdle}
+}
+
+Profile.init =
+function (myid, outfile)
+  if outfile then
+    out_ = io.open("./AI/USER_AI/"..outfile,"w")
+  end
+  return ftran, stIdleFollow, {myid}
+end
+
+return Profile
---
+++ USER_AI/profiles/ragefarm.lua
@@ -0,0 +1,177 @@
+require "./AI/USER_AI/const.lua"
+require "./AI/USER_AI/util.lua"
+require "./AI/USER_AI/states/atk.lua"
+require "./AI/USER_AI/states/idle.lua"
+require "./AI/USER_AI/states/commands.lua"
+require "./AI/USER_AI/states/follow.lua"
+
+Profile = {}
+-- Useful functions
+function isAttackable(myid, mob_data) -- Depends on mob's target
+  return mob_data.target == myid or ((mob_data.target == 0 or mob_data.target == -1 or isMob(mob_data.target)))
+end
+
+function nextTarget(myid, memory)
+  for id, mem_data in pairs(memory) do
+    if(isMob(id)) then
+      return {stFollowTarget, {myid, id, memory}}
+    end
+  end
+  return {stIdle, {myid}}
+end
+
+-- Transitions
+local ftran = {}
+
+ftran[stIdle] =
+  function (myid)
+    local owner = GetV(V_OWNER, myid)
+    local actors = getActors(V_TARGET, V_ATTACKRANGE, 
+      function (act, id)
+        if(id ~= myid and id ~= owner and isMob(id) and not isDead(id)) then
+          if(act[id].target == myid or act[id].target == owner) then
+	    return {stFollowTarget, {myid, id, {[owner] = act[id].target == owner}}}
+	  end
+	  act[id].dist = getDistance2(myid, id)
+	  if(isAttackable(myid, act[id]) and (act.dist == nil or act.dist > act[id].dist)) then
+	    act.dist = act[id].dist
+	    act.id = id
+	  end
+        end
+        return nil
+      end)
+    if(actors[myid] == nil) then
+      return unpack(actors)
+    end
+    local id = actors.id
+    if(id ~= nil and  getDistance2(owner, id) <= 12-actors[id].atkrange) then
+      return stFollowTarget, {myid, id, {}}
+    end
+    if(GetV(V_MOTION, owner) == MOTION_MOVE or getDistance2(myid, owner) > 4) then
+      return stIdleFollow, {myid}
+    end
+    return nil, nil
+  end
+
+ftran[stIdleFollow] =
+  function (myid)
+    local owner = GetV(V_OWNER, myid)
+    if(GetV(V_MOTION, owner) ~= MOTION_MOVE) then
+      return stIdle, {myid}
+    end
+    return nil, nil
+  end
+
+ftran[stFollowTarget] =
+  function (myid, target, memory)
+    local target_t = GetV(V_TARGET, target)
+    local owner = GetV(V_OWNER, myid)
+    if(isDead(target) or (target_t ~= myid and target_t ~= owner and not isMob(target_t) and target_t ~= 0 and target_t ~= -1)) then
+      return unpack(nextTarget(myid, memory))
+    end
+    if(getDistance2(myid, target) <= 2) then
+      SkillObject(myid, 3, 8004, myid)
+      memory[owner] = false
+      return stLifAttack, {myid, target, memory}
+    end
+    local actors = getActors(V_TARGET, V_HOMUNTYPE,
+      function (act, id)
+        if(isMob(id) and not isDead(id)) then
+          if(id ~= target and act[id].target == owner) then
+	    memory[owner] = true
+	    return {stFollowTarget, {myid, id, memory}}
+	  end
+	  if(id ~= target and isAttackable(myid, act[id]) and getDistance2(myid, id) < getDistance2(myid, target) and not memory[owner] and memory[id] == nil) then
+	    return {stFollowTarget, {myid, id, memory}}
+	  end
+	elseif(not isDead(id) and act[id].target == target and id ~= owner and id ~= myid) then
+	  return nextTarget(myid, memory)
+	end
+	return nil
+      end)
+    if(actors[myid] == nil) then
+      return unpack(actors)
+    end
+    if(getDistance2(owner, target) > 14-GetV(V_ATTACKRANGE, target)) then
+      return unpack(nextTarget(myid, memory))
+    end
+    return nil, nil
+  end
+
+ftran[stLifAttack] =
+  function (myid, target, memory)
+    if(isDead(target)) then
+      memory[target] = nil
+      return unpack(nextTarget(myid, memory))
+    end
+    local t_motion = GetV(V_MOTION, target) == MOTION_HIT
+    if(memory[target] == nil) then
+      memory[target] = 0
+    elseif(t_motion) then
+      memory[target] = memory[target] + 1
+    end
+    local owner = GetV(V_OWNER, myid)
+    local actors = getActors(V_TARGET, V_HOMUNTYPE, V_ATTACKRANGE,
+      function (act, id)
+        if(isMob(id) and not isDead(id)) then
+	  if(id ~= target) then
+	    if(act[id].target == owner) then
+	      memory[owner] = true
+	      return {stFollowTarget, {myid, id, memory}}
+	    end
+	    if(isAttackable(myid, act[id]) and getDistance2(myid, id) < 6 and getDistance2(owner, id) < 14-act[id].atkrange and memory[id] == nil and not memory[owner]) then
+	      return {stFollowTarget, {myid, id, memory}}
+	    end
+	    if(t_motion and memory[id] ~= nil and (act.hits == nil or memory[id] < act.hits)) then
+	      if memory[target] <= 0 and memory[id] > 0 then
+	        memory[id] = 0
+	      else
+	        memory[id] = memory[id] -1
+	      end
+	      act.hits = memory[id]
+	      act.id = id
+	    end
+	    if(isDead(id) and memory[id] ~= nil) then
+	      memory[id] = nil
+	    end
+	  end
+	else
+	if(not isDead(id) and id ~= myid and id ~= owner and isMob(act[id].target) and memory[act[id].target] ~= nil) then
+	    memory[act[id].target] = 0
+	    return {stLifAttack, {myid, act[id].target, memory}}
+	  end
+	end
+	return nil
+      end)
+    if(actors[myid] == nil) then
+      return unpack(actors)
+    end
+    if(actors.id ~= nil and actors.hits < memory[target]) then
+      return stFollowTarget, {myid, actors.id, memory}
+    end
+    return nil, nil
+  end
+
+ftran[stAttackCMD] =
+  function (myid, target)
+    if(isDead(target)) then
+      local x,y = GetV(V_POSITION, myid)
+      return stMoveCMD, {myid, x, y}
+    end
+    return nil, nil
+  end
+
+-- Commands
+Profile.command = {
+  [MOVE_CMD] = stMoveCMD,
+  [ATTACK_CMD] = stAttackCMD,
+  [FOLLOW_CMD] = {follow = stFollowCMD, unfollow = stIdle}
+}
+
+-- Init
+Profile.init =
+function (myid)
+  return ftran, stFollowCMD, {myid} -- ftran, fun, farg
+end
+
+return Profile
